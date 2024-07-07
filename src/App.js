c2041582f69da22c04b2a3d42a3371aa92b78dcf
mlrv9c@umsystem.edu <mlrv9c@umsl.edu>
2024-07-07 17:00:04 -0500

    Add placeholder haiku

--- src/App.js
+++ src/App.js
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Website in progress.
+          <br/> The site will work in due time.
+          <br/> Please check back later.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
