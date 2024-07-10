0536c718197573209522d660636070b934377442
mlrv9c@umsystem.edu <mlrv9c@umsl.edu>
2024-07-10 17:28:19 -0500

    Revert background color for 404 page back to default

--- src/router/NoPage.js
+++ src/router/NoPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function NoPage() {
     return (
-        <div className="w3-animate-opacity no-page">
+        <div className="w3-animate-opacity">
             <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
             <h1><b>Not Found</b></h1>
             <p>The page you are looking for does not exist.</p>
