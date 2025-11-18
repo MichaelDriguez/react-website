fdc05aec3fd93376c7e9466ca1e901822c1c4ab3
Michael Rodriguez <168235723+MichaelDriguez@users.noreply.github.com>
2025-11-18 12:25:06 -0600

    Update Page Design
    Changed aesthetic of website.

---
+++ router/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="w3-animate-opacity">
+      <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+      <h1 className='title'>WELCOME</h1>
+      <p>You have entered the website of Michael Rodriguez.</p>
+    </div>
+  )
+};
+
+export default Home;
---
+++ router/NoPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NoPage() {
+  return (
+    <div className="w3-animate-opacity">
+      <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+        <h1 className='title'>PAGE NOT FOUND</h1>
+      <p>The page you are looking for does not exist. <br/> <br/> ERROR 404</p>
+    </div>
+  );
+}
+
+export default NoPage;
