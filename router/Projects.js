8d374ca45f5a4d13e77dfc5dec9d1259ebc9e0e2
Michael Rodriguez <168235723+MichaelDriguez@users.noreply.github.com>
2026-01-28 00:52:05 -0600

    Update Projects.js
    - Added a new project (Raspberry Pi 4 Penetration Testing Lab).
    - Removed Password School.
    - Updated information for Virtual Machine Penetration Testing Lab.

--- router/Projects.js
+++ router/Projects.js
@@ -21,8 +21,8 @@ const Projects = () => {
         <div className="w3-animate-opacity">
           <br/> <br/>
           <h2><a href="https://github.com/MichaelDriguez/react-website"
-                 target="_blank" rel="noreferrer" className="hover-two"><b>React Portfolio Website</b></a>
-            <br/>July 2024 | <i className="devicon-html5-plain" style={{marginRight: '0.3%'}}></i>
+            target="_blank" rel="noreferrer" className="hover-two">
+            <b>React Portfolio Website</b></a> | <i className="devicon-html5-plain" style={{marginRight: '0.3%'}}></i>
             <i className="devicon-css3-plain" style={{marginRight: '0.3%'}}></i>
             <i className="devicon-javascript-plain" style={{marginRight: '0.3%'}}></i>
             <i className="devicon-nodejs-plain" style={{marginRight: '0.3%'}}></i>
@@ -35,18 +35,29 @@ const Projects = () => {
         </div>
       </div>
       <hr/>
-
       <div>
         <div className="w3-animate-opacity">
           <br/> <br/>
-          <h2><b>Penetration Testing Lab</b>
-            <br/>January 2024</h2>
-          <p>The penetration testing lab is an ongoing project. The lab itself is an isolated
-            virtual network where the attacker (Kali Linux) exploits the target machine (Metasploit 2).
-            Some of the tools I have used in the penetration testing lab include:
+          <h2><a href="https://github.com/MichaelDriguez/pi-4-pentesting-lab"
+                 target="_blank" rel="noreferrer" className="hover-two">
+                 <b>Raspberry Pi 4 Penetration Testing Lab</b></a> | <i className="devicon-kalilinux-original"></i>
+                 <i class="devicon-raspberrypi-plain"></i></h2>
+          <p>This repository contains documentation for my pentesting lab using Kali Linux XFCE.
+            A successor to my Virtual Machine Penetration Testing Lab, this lab reflects improved
+            documentation skills and more effective use of a variety of cybersecurity tools.
           </p>
+          <br/> <br/>
+        </div>
+      </div>
+      <div>
+        <div className="w3-animate-opacity">
+          <br/> <br/>
+          <h2><b>Virtual Machine Penetration Testing Lab</b> | <i className="devicon-kalilinux-original"></i></h2>
+          <p>The lab is an isolated virtual network where the attacker (Kali Linux)
+            exploits the target machine (Metasploit 2). Some of the tools I have used in the
+            pentesting lab include:</p>
           <ul>
-            <li><b>VirtualBox - </b> Used to set up the penetration testing environment itself.</li>
+            <li><b>VirtualBox - </b> Used to set up the pentesting environment itself.</li>
             <li>
               <b>Nmap - </b>Used to discover other machines on the network
               (in this case, just the target machine).
@@ -62,6 +73,10 @@ const Projects = () => {
               <b>Hydra - </b>Used to decrypt the target machine's hashed password.
             </li>
           </ul>
+          <p>This lab, which I made in early 2024, shows my early work in pentesting.
+            Since then, I continued improving these skills through school and independent work. My successor lab, the Raspberry Pi 4 Penetration
+            Testing Lab, reflects improvement on documentation and more effective use of some of the aforementioned tools.
+          </p>
         </div>
       </div>
       <br/> <br/>
@@ -69,8 +84,8 @@ const Projects = () => {
         <div className="w3-animate-opacity">
           <br/> <br/>
           <h2><a href="https://github.com/MichaelDriguez/secure-password-generator"
-                 target="_blank" rel="noreferrer" className="hover-two"><b>Secure Password Generator</b></a>
-            <br/>June 2024 | <i className="devicon-java-plain"></i></h2>
+                 target="_blank" rel="noreferrer" className="hover-two">
+                 <b>Secure Password Generator</b></a> | <i className="devicon-java-plain"></i></h2>
           <p>This program generates secure passwords that one could use for their accounts. This
             program includes several customization options for password generation,
             including password length, the inclusion of symbols, and several other options. </p>
@@ -81,11 +96,10 @@ const Projects = () => {
         <div className="w3-animate-opacity">
           <br/> <br/>
           <h2><a href="https://github.com/MichaelDriguez/password-strength-checker"
-                 target="_blank" rel="noreferrer" className="hover-two">
-            <b>Password Strength Checker</b></a><br/>July 2024 | <i className="devicon-bash-plain"></i>
-          </h2>
+          target="_blank" rel="noreferrer" className="hover-two">
+          <b>Password Strength Checker</b></a> | <i className="devicon-bash-plain"></i></h2>
           <p><b>In collaboration with </b><a href="https://github.com/jvalenteros"
-                                             target="_blank" rel="noreferrer" className="hover-two"><b>Johann
+            target="_blank" rel="noreferrer" className="hover-two"><b>Johann
             Valenteros</b></a>
           </p>
           <p>This program checks how secure your password is. It determines the score by checking the length
@@ -98,37 +112,15 @@ const Projects = () => {
       <div>
         <div className="w3-animate-opacity">
           <br/> <br/>
-          <h2><a href="https://github.com/MichaelDriguez/password-school"
-                 target="_blank" rel="noreferrer" className="hover-two"><b>Password School</b></a>
-            <br/>July 2024, December 2024 | <i className="devicon-html5-plain" style={{marginRight: '0.3%'}}></i>
-            <i className="devicon-css3-plain" style={{marginRight: '0.3%'}}></i>
-            <i className="devicon-tailwindcss-original" style={{marginRight: '0.3%'}}></i>
-            <i className="devicon-javascript-plain" style={{marginRight: '0.5%'}}></i></h2>
-          <p>
-            <a href="https://michaeldriguez.github.io/password-school/"
-               target="_blank" rel="noreferrer" className="hover-two"><b> Password School </b></a>
-            is a web version of the above two programs (the Password Strength Checker
-            and the Secure Password Generator).
-          </p>
-          <br/> <br/>
-        </div>
-      </div>
-      <div>
-        <div className="w3-animate-opacity">
-          <br/> <br/>
           <h2><a href="https://github.com/jvalenteros/ciphers"
-                 target="_blank" rel="noreferrer" className="hover-two"><b>Cipher Tools</b></a>
-            <br/>August 2024 | <i className="devicon-html5-plain" style={{marginRight: '0.3%'}}></i>
+            target="_blank" rel="noreferrer" className="hover-two">
+            <b>Cipher Tools</b></a> | <i className="devicon-html5-plain" style={{marginRight: '0.3%'}}></i>
             <i className="devicon-tailwindcss-original" style={{marginRight: '0.3%'}}></i>
             <i className="devicon-javascript-plain" style={{marginRight: '0.5%'}}></i></h2>
           <p><b>In collaboration with </b><a href="https://github.com/jvalenteros"
             target="_blank" rel="noreferrer" className="hover-two"><b>Johann Valenteros</b></a>
           </p>
-          <p>This program encodes and decodes text using various ciphers. As of August 5, 2024,
-            <a href="https://jvalenteros.github.io/ciphers/"
-               target="_blank" rel="noreferrer" className="hover-two"><b> Cipher Tools</b></a> supports
-            the ROT13 cipher and the Caesar cipher.
-          </p>
+          <p>This program encodes and decodes text using various ciphers.</p>
           <br/> <br/> <br/> <br/> <br/>
         </div>
       </div>
